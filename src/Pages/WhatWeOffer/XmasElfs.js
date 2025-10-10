6f10a55b30f7389d03a91db94c9e51bc12c22015
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2025-10-10 09:02:56 +0100

    elfs info added

--- src/Pages/WhatWeOffer/XmasElfs.js
+++ src/Pages/WhatWeOffer/XmasElfs.js
@@ -11,41 +11,36 @@ import xmasBalloon1 from '../../../src/Assets/Photos/ElfImages/xmasBalloon1.webp
 import xmasBalloon2 from '../../../src/Assets/Photos/ElfImages/xmasBalloon2.webp';
 import xmasBalloon3 from '../../../src/Assets/Photos/ElfImages/xmasBalloon3.webp';
 import xmasBalloon4 from '../../../src/Assets/Photos/ElfImages/xmasBalloon4.webp';
-import xmasBalloon5 from '../../../src/Assets/Photos/ElfImages/xmasBalloon5.webp';
-import xmasBalloon6 from '../../../src/Assets/Photos/ElfImages/xmasBalloon6.webp';
 
 const elfItems = [
 	{
 		id: 1,
 		title: 'Elf Balloon Garland 1',
+		description: 'Description Coming Soon',
 		price: 'Price Coming Soon',
 		img: xmasBalloon1,
 	},
 	{
 		id: 2,
 		title: 'Elf Balloon Garland 2',
+		description: 'Description Coming Soon',
 		price: 'Price Coming Soon',
 		img: xmasBalloon2,
 	},
 	{
 		id: 3,
 		title: 'Elf Balloon Garland 3',
+		description: 'Description Coming Soon',
 		price: 'Price Coming Soon',
 		img: xmasBalloon3,
 	},
 	{
 		id: 4,
 		title: 'Elf Balloon Garland 4',
+		description: 'Description Coming Soon',
 		price: 'Price Coming Soon',
 		img: xmasBalloon4,
 	},
-	{
-		id: 5,
-		title: 'Elf Balloon Garland 5',
-		price: 'Price Coming Soon',
-		img: xmasBalloon5,
-	},
-	{ id: 6, title: 'Elf Balloon Garland 6', price: '£175', img: xmasBalloon6 },
 ];
 
 export default function XmasElfs() {
@@ -53,7 +48,7 @@ export default function XmasElfs() {
 		<Box>
 			<Grid container spacing={4} sx={{ mt: 2 }}>
 				{elfItems.map((item) => (
-					<Grid item size={{ xs: 12, md: 4, lg: 2 }} key={item.id}>
+					<Grid item size={{ xs: 12, md: 4, lg: 3 }} key={item.id}>
 						<Card sx={{ borderRadius: '16px', boxShadow: 3 }}>
 							<CardMedia
 								component='img'
@@ -65,6 +60,9 @@ export default function XmasElfs() {
 							<CardContent sx={{ textAlign: 'center', background: '#f8f5f0' }}>
 								<Typography variant='h6' component='div'>
 									{item.title}
+								</Typography>
+								<Typography variant='body2' color='text.secondary'>
+									{item.description}
 								</Typography>
 								<Typography variant='subtitle1' sx={{ fontWeight: 'bold' }}>
 									{item.price}
